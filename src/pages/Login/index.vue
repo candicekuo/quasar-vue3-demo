e58a529ca19b8a5d5adb369ab9dda67108f23c32
Candice Kuo <killua7812@gmail.com>
2022-06-22 17:52:49 +0800

    login UI update

--- src/pages/Login/index.vue
+++ src/pages/Login/index.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="flex flex-center">
-    <p>login ⌓ ‿ ⌓</p>
+    <div>
+      <p>login ⌓ ‿ ⌓</p>
+      <p>{{ t('success') }}</p>
+      <template v-for="(data, index) in userData" :key="index">
+        <q-input v-model="data.val" outlined :label="data.label" dense :type="data.type" />
+      </template>
+      <q-select
+        v-model="lang"
+        :options="langOptions"
+        :label="t('Lang')"
+        emit-value
+        outlined
+        dense
+        @update:model-value="handleLangChange"
+      />
+
+      <q-btn color="primary" label="login" @click="login" />
+    </div>
   </div>
-  <p>{{ t('success') }}</p>
-  <div class="row">
-    <q-select
-      v-model="lang"
-      :options="langOptions"
-      :label="t('Lang')"
-      emit-value
-      class="col-3"
-      @update:model-value="handleLangChange"
-    />
-  </div>
-  <q-btn color="primary" label="login" @click="login" />
 </template>
 
 <script setup>
@@ -24,12 +29,25 @@ import { useRouter } from 'vue-router';
 const { t, locale } = useI18n();
 const router = useRouter();
 
-const lang = ref({ value: 'en-US', label: t('Lang EN') });
-
 const langOptions = reactive([
   { value: 'en-US', label: t('Lang EN') },
   { value: 'zh-TW', label: t('Lang ZH') },
 ]);
+const defaultLang = reactive(langOptions.find((item) => item.value === locale.value));
+const lang = ref(defaultLang);
+
+const userData = reactive({
+  id: {
+    label: 'ID',
+    val: null,
+    type: 'text',
+  },
+  psw: {
+    label: 'Password',
+    val: null,
+    type: 'password',
+  },
+});
 
 const handleLangChange = (lang) => {
   locale.value = lang;
